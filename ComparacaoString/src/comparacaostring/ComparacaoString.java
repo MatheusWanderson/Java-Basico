18225955cf889f2c14f7fc8b077c34cc65551bcc
Wanderson Matheus da Silva <wandersonmatheus@outlook.com.br>
2021-09-11 22:48:45 -0300

    aula08_java

--- ComparacaoString/src/comparacaostring/ComparacaoString.java
+++ ComparacaoString/src/comparacaostring/ComparacaoString.java
@@ -16,11 +16,18 @@ public class ComparacaoString {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String nome1 = "Gustavo";
+        /*String nome1 = "Gustavo";
         String nome2 = "Gustavo";
         String nome3 = new String ("Gustavo");
         String res;
         res = (nome1==nome2)?"igual":"diferente";
+        System.out.println(res);*/
+        
+        String nome1 = "Gustavo";
+        String nome2 = "Gustavo";
+        String nome3 = new String ("Gustavo");
+        String res;
+        res = (nome1.equals(nome3))?"igual":"diferente";
         System.out.println(res);
     }
     
