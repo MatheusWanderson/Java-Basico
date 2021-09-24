95ea74be54615d6ca473747644d8c5658847d383
Wanderson Matheus da Silva <wandersonmatheus@outlook.com.br>
2021-09-23 22:02:20 -0300

    Finalizando_aula08_CursoEmVideo

--- LeitorDePensamento/src/classes/telaGenio.java
+++ LeitorDePensamento/src/classes/telaGenio.java
@@ -5,6 +5,8 @@
  */
 package classes;
 
+import java.awt.Font;
+
 /**
  *
  * @author wande
@@ -16,6 +18,7 @@ public class telaGenio extends javax.swing.JFrame {
      */
     public telaGenio() {
         initComponents();
+        lblFrase.setText("<html>Irei pensar em um valor entre <strong>1 e 5</strong>. Tente adivinhar</html>");
     }
 
     /**
@@ -28,20 +31,58 @@ public class telaGenio extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel5 = new javax.swing.JLabel();
+        lblFrase = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        txtVal = new javax.swing.JSpinner();
+        btnPalpite = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/genio1.png"))); // NOI18N
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 210, 136, 144));
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 220, 140, 144));
+
+        lblFrase.setText("Frase");
+        lblFrase.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        getContentPane().add(lblFrase, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 110, 90, 80));
+
+        jLabel3.setText("Valor");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 280, -1, -1));
+
+        txtVal.setModel(new javax.swing.SpinnerNumberModel(1, 1, 5, 1));
+        getContentPane().add(txtVal, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 270, 60, 30));
+
+        btnPalpite.setText("Palpite");
+        btnPalpite.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPalpiteActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnPalpite, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 310, -1, -1));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/balao2..png"))); // NOI18N
-        jLabel1.setText("jLabel1");
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 40, -1, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 40, 330, 220));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPalpiteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPalpiteActionPerformed
+        // TODO add your handling code here:
+        double n = 1 + Math.random() * (6-1);
+        int valor = (int) n;
+        
+        int num = Integer.parseInt(txtVal.getValue().toString());
+        
+        String f1 = "<html>ACERTOU!</html>";
+        String f2 = "<html>ERROU! Eu pensei no valor " + valor + "</html>";
+        
+        String res = (valor==num)?f1:f2;
+        
+        lblFrase.setFont(new Font("Arial Black", Font.PLAIN, 14));
+        
+        lblFrase.setText(res);
+    }//GEN-LAST:event_btnPalpiteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,7 +120,11 @@ public class telaGenio extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnPalpite;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel lblFrase;
+    private javax.swing.JSpinner txtVal;
     // End of variables declaration//GEN-END:variables
 }
